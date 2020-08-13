cec14ede33c89c7f64d5dc799538c9ff4477ee51
theADAMJR <adamjf123@gmail.com>
2020-08-13 14:13:53 +0100

    semi refactor

---
+++ Data/Structs/Config.cs
@@ -0,0 +1,15 @@
+﻿using Discord;
+
+namespace Bot3PG.Data.Structs
+{
+    public class Config
+    {
+        public struct Bot {
+            public string Token { get; set; }
+            public string Status { get; set; }
+        }
+        public string DashboardURL { get; set; }
+        public LogSeverity LogSeverity { get; set; } = LogSeverity.Verbose;
+        public string MongoURI { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Threading.Tasks;
+using Bot3PG.Handlers;
+
+namespace Bot3PG
+{
+    public class Program
+    {
+        private static Task Main(string[] args) => new DiscordHandler().InitializeAsync();
+    }
+}
